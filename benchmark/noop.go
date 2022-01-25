5e863b948d6df6fb3f9722a9ae8b16667fb921ff
PasinduTennage <pasindu.13@cse.mrt.ac.lk>
2022-01-25 19:58:57 +0100

    Refactored noop,go

--- benchmark/noop.go
+++ benchmark/noop.go
@@ -3,12 +3,16 @@ package benchmark
 import "time"
 
 /*
-No Op application implements an echo service where the client response is equal to the request. It also defines a synthetic service time, where the response is delayed until the time
+	No Op application implements an echo service where the client response is equal to the request. It also defines a synthetic service time, where the response is delayed until the time
 */
 
 type NoOpApplication struct {
 	SleepDuration int64 // sleep time in micro seconds
 }
+
+/*
+	Execute the no-op app
+*/
 
 func (sleepApp *NoOpApplication) executeNoOpApp(request string) string {
 	start := time.Now()
