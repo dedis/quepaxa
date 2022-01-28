97ff6de6482632662d99b8d3448a7baf597b7fe1
PasinduTennage <pasindu.13@cse.mrt.ac.lk>
2022-01-28 19:30:19 +0100

    Added and tested remote test

--- experiments/remote/remote-test-overlay.sh
+++ experiments/remote/remote-test-overlay.sh
@@ -1,9 +1,8 @@
 arrivalRate=$1
-iteration=$2
 
 remote_algo_path="/raxos/home/oem/GolandProjects/raxos/logs/raxos/replica/bin/replica"
 remote_ctl_path="/raxos/home/oem/GolandProjects/raxos/logs/raxos/client/bin/client"
-remote_doc_path="/home/pasindu/raxos/home/oem/GolandProjects/raxos/logs/raxos/logs/"
+remote_log_path="/home/pasindu/raxos/home/oem/GolandProjects/raxos/logs/raxos/logs/"
 remote_config_path="/home/pasindu/raxos/home/oem/GolandProjects/raxos/logs/raxos/configuration/remote/configuration.yml"
 output_path="/home/oem/GolandProjects/raxos/logs/"
 
@@ -54,65 +53,66 @@ rm ${output_path}9.log
 rm ${output_path}status1.log
 rm ${output_path}status2.log
 rm ${output_path}pythonreport.log
+rm ${output_path}test.log
 
 echo "Removed old log files"
 
-sshpass ssh -i ${replica1_cert}  ${replica1} "rm ${remote_doc_path}0.txt"
-sshpass ssh -i ${replica2_cert}  ${replica2} "rm ${remote_doc_path}1.txt"
-sshpass ssh -i ${replica3_cert}  ${replica3} "rm ${remote_doc_path}2.txt"
-sshpass ssh -i ${replica4_cert}  ${replica4} "rm ${remote_doc_path}3.txt"
-sshpass ssh -i ${replica5_cert}  ${replica5} "rm ${remote_doc_path}4.txt"
+sshpass ssh -i ${replica1_cert}  ${replica1} "rm ${remote_log_path}0.txt"
+sshpass ssh -i ${replica2_cert}  ${replica2} "rm ${remote_log_path}1.txt"
+sshpass ssh -i ${replica3_cert}  ${replica3} "rm ${remote_log_path}2.txt"
+sshpass ssh -i ${replica4_cert}  ${replica4} "rm ${remote_log_path}3.txt"
+sshpass ssh -i ${replica5_cert}  ${replica5} "rm ${remote_log_path}4.txt"
 
 
-sshpass ssh -i ${client1_cert}  ${client1} "rm ${remote_doc_path}5.txt"
-sshpass ssh -i ${client2_cert}  ${client2} "rm ${remote_doc_path}6.txt"
-sshpass ssh -i ${client3_cert}  ${client3} "rm ${remote_doc_path}7.txt"
-sshpass ssh -i ${client4_cert}  ${client4} "rm ${remote_doc_path}8.txt"
-sshpass ssh -i ${client5_cert}  ${client5} "rm ${remote_doc_path}9.txt"
- 
+sshpass ssh -i ${client1_cert}  ${client1} "rm ${remote_log_path}5.txt"
+sshpass ssh -i ${client2_cert}  ${client2} "rm ${remote_log_path}6.txt"
+sshpass ssh -i ${client3_cert}  ${client3} "rm ${remote_log_path}7.txt"
+sshpass ssh -i ${client4_cert}  ${client4} "rm ${remote_log_path}8.txt"
+sshpass ssh -i ${client5_cert}  ${client5} "rm ${remote_log_path}9.txt"
+
 sleep 5
 echo "Removed all files in remote servers"
 
 kill_command="pkill replica ; pkill client"
 
-sshpass ssh -i ${replica1_cert}  ${replica1} ${kill_command}
-sshpass ssh -i ${replica2_cert}  ${replica2} ${kill_command}
-sshpass ssh -i ${replica3_cert}  ${replica3} ${kill_command}
-sshpass ssh -i ${replica4_cert}  ${replica4} ${kill_command}
-sshpass ssh -i ${replica5_cert}  ${replica5} ${kill_command}
+sshpass ssh -i ${replica1_cert}  ${replica1} "${kill_command}"
+sshpass ssh -i ${replica2_cert}  ${replica2} "${kill_command}"
+sshpass ssh -i ${replica3_cert}  ${replica3} "${kill_command}"
+sshpass ssh -i ${replica4_cert}  ${replica4} "${kill_command}"
+sshpass ssh -i ${replica5_cert}  ${replica5} "${kill_command}"
 
 
-sshpass ssh -i ${client1_cert}  ${client1} ${kill_command}
-sshpass ssh -i ${client2_cert}  ${client2} ${kill_command}
-sshpass ssh -i ${client3_cert}  ${client3} ${kill_command}
-sshpass ssh -i ${client4_cert}  ${client4} ${kill_command}
-sshpass ssh -i ${client5_cert}  ${client5} ${kill_command}
+sshpass ssh -i ${client1_cert}  ${client1} "${kill_command}"
+sshpass ssh -i ${client2_cert}  ${client2} "${kill_command}"
+sshpass ssh -i ${client3_cert}  ${client3} "${kill_command}"
+sshpass ssh -i ${client4_cert}  ${client4} "${kill_command}"
+sshpass ssh -i ${client5_cert}  ${client5} "${kill_command}"
 
 echo "killed previous running instances"
 
 sleep 5
 
-nohup sshpass ssh -i ${replica1_cert} -n -f ${replica1} ".${remote_algo_path} --name 0 --config ${remote_config_path}  --logFilePath ${remote_doc_path} --batchSize 50 --batchTime 500 " > ${output_path}0.log &
-nohup sshpass ssh -i ${replica2_cert} -n -f ${replica2} ".${remote_algo_path} --name 1 --config ${remote_config_path}  --logFilePath ${remote_doc_path} --batchSize 50 --batchTime 500 " > ${output_path}1.log &
-nohup sshpass ssh -i ${replica3_cert} -n -f ${replica3} ".${remote_algo_path} --name 2 --config ${remote_config_path}  --logFilePath ${remote_doc_path} --batchSize 50 --batchTime 500 " > ${output_path}2.log &
-nohup sshpass ssh -i ${replica4_cert} -n -f ${replica4} ".${remote_algo_path} --name 3 --config ${remote_config_path}  --logFilePath ${remote_doc_path} --batchSize 50 --batchTime 500 " > ${output_path}3.log &
-nohup sshpass ssh -i ${replica5_cert} -n -f ${replica5} ".${remote_algo_path} --name 4 --config ${remote_config_path}  --logFilePath ${remote_doc_path} --batchSize 50 --batchTime 500 " > ${output_path}4.log &
-echo "Started servers, Please check the nohup.out"
+nohup sshpass ssh -i ${replica1_cert} -n -f ${replica1} ".${remote_algo_path} --name 0 --config ${remote_config_path}  --logFilePath ${remote_log_path} --batchSize 50 --batchTime 500 " > ${output_path}0.log &
+nohup sshpass ssh -i ${replica2_cert} -n -f ${replica2} ".${remote_algo_path} --name 1 --config ${remote_config_path}  --logFilePath ${remote_log_path} --batchSize 50 --batchTime 500 " > ${output_path}1.log &
+nohup sshpass ssh -i ${replica3_cert} -n -f ${replica3} ".${remote_algo_path} --name 2 --config ${remote_config_path}  --logFilePath ${remote_log_path} --batchSize 50 --batchTime 500 " > ${output_path}2.log &
+nohup sshpass ssh -i ${replica4_cert} -n -f ${replica4} ".${remote_algo_path} --name 3 --config ${remote_config_path}  --logFilePath ${remote_log_path} --batchSize 50 --batchTime 500 " > ${output_path}3.log &
+nohup sshpass ssh -i ${replica5_cert} -n -f ${replica5} ".${remote_algo_path} --name 4 --config ${remote_config_path}  --logFilePath ${remote_log_path} --batchSize 50 --batchTime 500 " > ${output_path}4.log &
+echo "Started servers"
 
 sleep 20
 
-sshpass ssh -i ${client1_cert}  ${client1} ".${remote_ctl_path} --name 5 --config ${remote_config_path} --logFilePath ${remote_doc_path} --requestType status --operationType 1" > ${output_path}status1.log
+sshpass ssh -i ${client1_cert}  ${client1} ".${remote_ctl_path} --name 5 --config ${remote_config_path} --logFilePath ${remote_log_path} --requestType status --operationType 1" > ${output_path}status1.log
 echo "Sent initial status"
 
 sleep 10
 
 echo "Starting client[s]"
 
-nohup sshpass ssh -i ${client1_cert}  ${client1} ".${remote_ctl_path} --name 5 --config ${remote_config_path} --logFilePath ${remote_doc_path} --requestType request --arrivalRate ${arrivalRate} " >${output_path}5.log &
-nohup sshpass ssh -i ${client2_cert}  ${client2} ".${remote_ctl_path} --name 6 --config ${remote_config_path} --logFilePath ${remote_doc_path} --requestType request --arrivalRate ${arrivalRate} " >${output_path}6.log &
-nohup sshpass ssh -i ${client3_cert}  ${client3} ".${remote_ctl_path} --name 7 --config ${remote_config_path} --logFilePath ${remote_doc_path} --requestType request --arrivalRate ${arrivalRate} " >${output_path}7.log &
-nohup sshpass ssh -i ${client4_cert}  ${client4} ".${remote_ctl_path} --name 8 --config ${remote_config_path} --logFilePath ${remote_doc_path} --requestType request --arrivalRate ${arrivalRate} " >${output_path}8.log &
-sshpass ssh -i ${client5_cert}  ${client5} ".${remote_ctl_path} --name 9 --config ${remote_config_path} --logFilePath ${remote_doc_path} --requestType request --arrivalRate ${arrivalRate} " >{output_path}9.log
+nohup sshpass ssh -i ${client1_cert}  ${client1} ".${remote_ctl_path} --name 5 --defaultReplica 0 --config ${remote_config_path} --logFilePath ${remote_log_path} --requestType request --arrivalRate ${arrivalRate} " >${output_path}5.log &
+nohup sshpass ssh -i ${client2_cert}  ${client2} ".${remote_ctl_path} --name 6  --defaultReplica 1 --config ${remote_config_path} --logFilePath ${remote_log_path} --requestType request --arrivalRate ${arrivalRate} " >${output_path}6.log &
+nohup sshpass ssh -i ${client3_cert}  ${client3} ".${remote_ctl_path} --name 7  --defaultReplica 2 --config ${remote_config_path} --logFilePath ${remote_log_path} --requestType request --arrivalRate ${arrivalRate} " >${output_path}7.log &
+nohup sshpass ssh -i ${client4_cert}  ${client4} ".${remote_ctl_path} --name 8  --defaultReplica 3 --config ${remote_config_path} --logFilePath ${remote_log_path} --requestType request --arrivalRate ${arrivalRate} " >${output_path}8.log &
+sshpass ssh -i ${client5_cert}  ${client5} ".${remote_ctl_path} --name 9 --defaultReplica 4  --config ${remote_config_path} --logFilePath ${remote_log_path} --requestType request --arrivalRate ${arrivalRate} " >${output_path}9.log
 
 sleep 10
 
@@ -121,46 +121,46 @@ echo "Completed Client[s]"
 
 sleep 10
 
-sshpass ssh -i ${client1_cert}  ${client1} ".${remote_ctl_path} --name 5 --config ${remote_config_path} --logFilePath ${remote_doc_path} --requestType status --operationType 2" > {output_path}status2.log
+sshpass ssh -i ${client1_cert}  ${client1} ".${remote_ctl_path} --name 5 --config ${remote_config_path} --logFilePath ${remote_log_path} --requestType status --operationType 2" >${output_path}status2.log
 echo "Sent status to print log"
 
 sleep 20
 
-scp -i ${replica1_cert}  ${replica1}:${remote_doc_path}0.txt {output_path}0.txt 
-scp -i ${replica2_cert}  ${replica2}:${remote_doc_path}1.txt {output_path}1.txt
-scp -i ${replica3_cert}  ${replica3}:${remote_doc_path}2.txt {output_path}2.txt
-scp -i ${replica4_cert}  ${replica4}:${remote_doc_path}3.txt {output_path}3.txt
-scp -i ${replica5_cert}  ${replica5}:${remote_doc_path}4.txt {output_path}4.txt
+scp -i ${replica1_cert}  ${replica1}:${remote_log_path}0.txt ${output_path}0.txt
+scp -i ${replica2_cert}  ${replica2}:${remote_log_path}1.txt ${output_path}1.txt
+scp -i ${replica3_cert}  ${replica3}:${remote_log_path}2.txt ${output_path}2.txt
+scp -i ${replica4_cert}  ${replica4}:${remote_log_path}3.txt ${output_path}3.txt
+scp -i ${replica5_cert}  ${replica5}:${remote_log_path}4.txt ${output_path}4.txt
 
 
-scp -i ${client1_cert}   ${client1}:${remote_doc_path}5.txt {output_path}5.txt 
-scp -i ${client2_cert}   ${client2}:${remote_doc_path}6.txt {output_path}6.txt 
-scp -i ${client3_cert}   ${client3}:${remote_doc_path}7.txt {output_path}7.txt 
-scp -i ${client4_cert}   ${client4}:${remote_doc_path}8.txt {output_path}8.txt 
-scp -i ${client5_cert}   ${client5}:${remote_doc_path}9.txt {output_path}9.txt 
+scp -i ${client1_cert}   ${client1}:${remote_log_path}5.txt ${output_path}5.txt
+scp -i ${client2_cert}   ${client2}:${remote_log_path}6.txt ${output_path}6.txt
+scp -i ${client3_cert}   ${client3}:${remote_log_path}7.txt ${output_path}7.txt
+scp -i ${client4_cert}   ${client4}:${remote_log_path}8.txt ${output_path}8.txt
+scp -i ${client5_cert}   ${client5}:${remote_log_path}9.txt ${output_path}9.txt
 
 echo "Copied all the files to local machine"
 
 sleep 5
 
-python3 /home/oem/GolandProjects/raxos/experiments/python/overlay-test.py {output_path}0.txt {output_path}1.txt {output_path}2.txt {output_path}3.txt {output_path}4.txt>{output_path}pythonreport.log     
+python3 /home/oem/GolandProjects/raxos/experiments/python/overlay-test.py ${output_path}0.txt ${output_path}1.txt ${output_path}2.txt ${output_path}3.txt ${output_path}4.txt >${output_path}pythonreport.log
 
 dst_directory="/home/oem/Desktop/Test/${arrivalRate}/"
-mkdir -p ${dst_directory}
-cp -r {output_path} ${dst_directory}
+mkdir -p "${dst_directory}"
+cp -r ${output_path} "${dst_directory}"
 
 sshpass ssh -i ${replica1_cert} ${replica1} "pkill replica"
 sshpass ssh -i ${replica2_cert} ${replica2} "pkill replica"
 sshpass ssh -i ${replica3_cert} ${replica3} "pkill replica"
 sshpass ssh -i ${replica4_cert} ${replica4} "pkill replica"
 sshpass ssh -i ${replica5_cert} ${replica5} "pkill replica"
- 
+
 sshpass ssh -i ${client1_cert} ${client1} "pkill client"
 sshpass ssh -i ${client2_cert} ${client2} "pkill client"
 sshpass ssh -i ${client3_cert} ${client3} "pkill client"
 sshpass ssh -i ${client4_cert} ${client4} "pkill client"
 sshpass ssh -i ${client5_cert} ${client5} "pkill client"
- 
+
 echo "killed  instances"
 
 echo "Finish test"
