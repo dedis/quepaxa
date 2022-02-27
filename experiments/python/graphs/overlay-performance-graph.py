b5274a7ce140257f4d0cd55584d1880854644519
pasindu <pasindu.13@cse.mr.ac.lk>
2022-02-27 18:45:57 +0100

    Basic consensus experiments done

--- experiments/python/graphs/overlay-performance-graph.py
+++ experiments/python/graphs/overlay-performance-graph.py
@@ -1,9 +1,13 @@
 import matplotlib.pyplot as plt
 
-throughput = [5001,25014, 49984,75008, 125094,274993,374954,500040,550055,599936]
-median_latency = [554,792.8,960.2,1260,1878.4,2923,4661.8,10840,14872.8,18774.4]
-ninetynine_latency = [6152.6,49942.6,49985.4,75357.4,89009.8,130943,287972.7,651230.7,742857.1,1176964.1]
+throughput_overlay = [5001, 25014, 49984, 75008, 125094, 274993, 374954, 500040, 550055, 599936]
+median_latency_overlay = [554, 792.8, 960.2, 1260, 1878.4, 2923, 4661.8, 10840, 14872.8, 18774.4]
+ninetynine_latency_overlay = [6152.6, 49942.6, 49985.4, 75357.4, 89009.8, 130943, 287972.7, 651230.7, 742857.1,
+                              1176964.1]
 
+throughput_basic_raxos = [4990, 25017, 49997, 99973, 200012, 299882, 399850, 499909, 549915]
+median_latency_raxos = [1938.2, 5224.8, 11551.4, 29241.5, 42868, 100261.4, 210789.6, 800948.2, 1249306.2]
+ninetynine_latency_raxos = [73599, 561568.5, 663196.9, 665300.9, 931013.6, 1302786, 3496800.7, 5591776.3, 5991776.3]
 # plt.rcParams.update({'font.size': 13.45})
 
 ax = plt.gca()
@@ -11,10 +15,15 @@ ax = plt.gca()
 # ax.set_xlim([0, 749865 + 10])
 # ax.set_ylim([0, 14200])
 
-plt.plot(throughput, ninetynine_latency, 'r*-', label="QSCOD")
+# plt.plot(throughput_overlay, median_latency_overlay, 'r*-', label="Overlay")
+# plt.plot(throughput_basic_raxos, median_latency_raxos, 'g*-', label="QSCOD")
+plt.plot(throughput_overlay, ninetynine_latency_overlay, 'r*-', label="Overlay")
+plt.plot(throughput_basic_raxos, ninetynine_latency_raxos, 'g*-', label="QSCOD")
 
 plt.xlabel('Throughput (requests per second)')
-plt.ylabel('99% latency (micro seconds)')
+# plt.ylabel('Median latency (micro seconds)')
+plt.ylabel('Tail latency 99% (micro seconds)')
 plt.legend()
 
-plt.savefig('/home/oem/Desktop/Test/tail_latency..png')
+# plt.savefig('/home/pasindu/Desktop/Test/median_latency.png')
+plt.savefig('/home/pasindu/Desktop/Test/tail_latency.png')
--- experiments/python/summary-report.py
+++ experiments/python/summary-report.py
@@ -4,6 +4,7 @@ num_clients = int(sys.argv[1])
 
 headers = ["Arrival Rate", "Error Rate", "Median Latency (micro seconds)", "99% latency (micro seconds)",
            "Throughput (requests per second)"]
+
 for i in range(1, num_clients + 1):
     headers.append("Client " + str(i) + " Throughput (requests per second)")
 
@@ -13,7 +14,7 @@ for i in range(1, num_clients + 1):
 # best case
 records = [headers]
 
-for arrivalrate in [1000, 5000, 10000, 15000, 25000, 55000, 75000, 100000, 110000, 120000]:
+for arrivalrate in [1000, 5000, 10000, 20000, 40000, 60000, 80000, 100000, 110000]:
     newRecord = [str(arrivalrate)]
     throughputs = []
     latency = []
@@ -21,11 +22,16 @@ for arrivalrate in [1000, 5000, 10000, 15000, 25000, 55000, 75000, 100000, 11000
     errors = []
 
     for client in range(5, num_clients + 5):
-        file_name = "/home/oem/Desktop/Test/" + str(arrivalrate) + "/logs/" + str(client) + ".log"
+        file_name = "/home/pasindu/Desktop/Test/" + str(arrivalrate) + "/logs/" + str(client) + ".log"
         with open(file_name) as f:
             content = f.readlines()
+
+        if content[0].strip().split(" ")[0] == "Warning:":
+            content = content[1:]
+
         if not (content[10].strip().split(" ")[0] == "Throughput" and content[11].strip().split(" ")[0] == "Median"):
             print("Error in " + file_name + "\n")
+            continue
         throughputs.append(float(content[10].strip().split(" ")[5]))
         latency.append(float(content[11].strip().split(" ")[6]))
         tail_latency.append(float(content[12].strip().split(" ")[7]))
@@ -44,10 +50,11 @@ for arrivalrate in [1000, 5000, 10000, 15000, 25000, 55000, 75000, 100000, 11000
         newRecord.append(t)
     for l in latency:
         newRecord.append(l)
+
     records.append(newRecord)
 
 import csv
 
-with open("/home/oem/Desktop/Test/best_case_tail.csv", "w", newline="") as f:
+with open("/home/pasindu/Desktop/Test/best_case.csv", "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerows(records)
