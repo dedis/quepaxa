becde776660214c17c3dd8797e6a323adf662d25
pasindu <pasindu.13@cse.mr.ac.lk>
2022-09-22 17:29:06 +0200

    removed smr proto files

--- magefile.go
+++ magefile.go
@@ -46,7 +46,6 @@ func Deps() error {
 func Generate() error {
 
 	err := sh.RunV("protoc", "--go_out=./", "--go_opt=paths=source_relative", "--go-grpc_out=.", "--go-grpc_opt=paths=source_relative", "./proto/consensus/consensus.proto")
-	err = sh.RunV("protoc", "--go_out=./", "./proto/smr.proto")
 	err = sh.RunV("protoc", "--go_out=./", "./proto/client.proto")
 
 	if err != nil {
