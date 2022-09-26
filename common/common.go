8cf04f3587b567ca9694c04ca777b5c26182f945
pasindu <pasindu.13@cse.mr.ac.lk>
2022-09-26 22:28:42 +0200

    changed and checked common.go

--- common/common.go
+++ common/common.go
@@ -1,1 +1,21 @@
 package common
+
+import "raxos/proto"
+
+/*
+	RPC pair assigns a unique id to each type of message defined in the proto files
+*/
+
+type RPCPair struct {
+	Code uint8
+	Obj  proto.Serializable
+}
+
+/*
+	Outgoing RPC assigns a rpc to its intended destination peer, the peer can be a replica or a client
+*/
+
+type OutgoingRPC struct {
+	RpcPair *RPCPair
+	Peer    int64
+}
