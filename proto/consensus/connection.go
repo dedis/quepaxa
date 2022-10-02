3142149fbebc24e4b065c46d99e98c21913707e9
pasindu <pasindu.13@cse.mr.ac.lk>
2022-10-02 16:13:47 +0200

    added recorder grpc listener

---
+++ proto/consensus/connection.go
@@ -0,0 +1,25 @@
+package consensus
+
+import (
+	"context"
+	raxos "raxos/replica/src"
+)
+
+type GRPCConnection struct {
+	Recorder *raxos.Recorder
+}
+
+// answer to proposer RPC
+
+func (gc *GRPCConnection) ESP(ctx context.Context, req *ProposerMessage) (*RecorderResponse, error) {
+
+	var response *RecorderResponse
+	response = gc.Recorder.HandleESP(req)
+	return response, nil
+}
+
+// for gRPC forward compatibility
+
+func (gc *GRPCConnection) mustEmbedUnimplementedConsensusServer() {
+
+}
--- replica/src/recorder.go
+++ replica/src/recorder.go
@@ -1,20 +1,28 @@
 package raxos
 
 import (
-	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"net"
+	"os"
 	"raxos/proto/consensus"
 )
 
 type Recorder struct {
-	// gRPC listener variables
+	address    string       // address to listen for gRPC connections
+	listener   net.Listener // socket for gRPC connections
+	server     *grpc.Server // gRPC server
+	connection *consensus.GRPCConnection
 	// pointer to the time array
 }
 
 // instantiate a new Recorder
 
-func NewRecorder() *Recorder {
+func NewRecorder(address string) *Recorder {
 
-	re := Recorder{}
+	re := Recorder{
+		address:  address,
+	}
 
 	return &re
 }
@@ -22,24 +30,38 @@ func NewRecorder() *Recorder {
 // start listening to gRPC connection
 
 func (r *Recorder) NetworkInit() {
+	r.server = grpc.NewServer()
+	r.connection = &consensus.GRPCConnection{Recorder: r}
+	consensus.RegisterConsensusServer(r.server, r.connection)
 
+	// start listener
+	listener, err := net.Listen("tcp", r.address)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "listen: %v", err)
+		os.Exit(1)
+	}
+	err = r.server.Serve(listener)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "serve: %v", err)
+		os.Exit(1)
+	}
 }
 
 // answer to proposer RPC
 
-func (re *Recorder) ESP(ctx context.Context, req *consensus.ProposerMessage) (*consensus.RecorderResponse, error) {
+func (re *Recorder) HandleESP(req *consensus.ProposerMessage) *consensus.RecorderResponse {
 
 	var response consensus.RecorderResponse
-	
+
 	// todo
-	
+
 	// send the last decided index details to the proxy, if available
-	
+
 	// if there are only hashes, then check if all the client batches are available in the shared pool. if yes send a positive response depending on the consensus rules
-	
+
 	// if not send a negative response
-	
+
 	// Mark the time of the propose message for the proposer
-	
-	return &response, nil
+
+	return &response
 }
