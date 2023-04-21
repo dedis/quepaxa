e6c2b2e910682335f0c083738b962c4a8cee2075
Bryan A Ford <brynosaurus@gmail.com>
2023-04-21 08:45:28 +0200

    run.sh

--- model-checker/run.sh
+++ model-checker/run.sh
@@ -3,7 +3,7 @@
 
 # Exhaustive verification.
 # MEMLIMIT is the memory-usage limit in megabytes.
-spin -search -O2 -safety -DMEMLIM=60000 $1
+#spin -search -O2 -safety -DMEMLIM=60000 $1
 
 # Set maximum search depth (-m), making it an error to exceed this depth (-b).
 #spin -search -O2 -safety -DMEMLIM=60000 -m3870 -b $1
@@ -16,5 +16,5 @@ spin -search -O2 -safety -DMEMLIM=60000 $1
 # -w defines the power of two of the hash table size in bits.
 #	examples: -w28: 32MB, -w33: 1GB, -w38: 32GB
 #spin -search -O2 -safety -bitstate -w28 $1
-#spin -search -O2 -safety -bitstate -w38 $1
+spin -search -O2 -safety -bitstate -w38 $1
 
