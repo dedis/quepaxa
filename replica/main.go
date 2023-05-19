66b9ee572a976a4d215909a8b9e77a7437080767
Pasindu Tennage <pasindu.13@cse.mrt.ac.lk>
2023-05-19 20:41:23 +0200

    commenting replica

--- replica/main.go
+++ replica/main.go
@@ -9,18 +9,20 @@ import (
 	"time"
 )
 
+// this file defines the main routine of QuePaxa, which takes input arguments from the command line
+
 func main() {
-	configFile := flag.String("config", "configuration/local/configuration.yml", "raxos configuration file")
+	configFile := flag.String("config", "configuration/local/configuration.yml", "QuePaxa configuration file")
 	name := flag.Int64("name", 1, "name of the replica")
 	logFilePath := flag.String("logFilePath", "logs/", "log file path")
 	batchSize := flag.Int64("batchSize", 50, "replica batch size")
 	batchTime := flag.Int64("batchTime", 5000, "replica batch time in micro seconds")
 	leaderTimeout := flag.Int64("leaderTimeout", 5000000, "leader timeout in micro seconds")
-	pipelineLength := flag.Int64("pipelineLength", 50, "pipeline length maximum number of outstanding proposals")
+	pipelineLength := flag.Int64("pipelineLength", 1, "pipeline length maximum number of outstanding proposals")
 	benchmark := flag.Int64("benchmark", 0, "Benchmark: 0 for KV store and 1 for Redis ")
 	debugOn := flag.Bool("debugOn", false, "true / false")
-	debugLevel := flag.Int("debugLevel", 0, "debug level")
-	leaderMode := flag.Int("leaderMode", 0, "mode of leader change: 0 for fixed leader order, 1 for fixed order, static partition,  2 for M.A.B based on commit times, 3 for asynchronous")
+	debugLevel := flag.Int("debugLevel", 1010, "debug level")
+	leaderMode := flag.Int("leaderMode", 0, "mode of leader change: 0 for fixed leader order, 1 for round robin, static partition,  2 for M.A.B based on commit times, 3 for asynchronous, 4 for last committed proposer")
 	serverMode := flag.Int("serverMode", 0, "0 for non-lan-optimized, 1 for lan optimized")
 	epochSize := flag.Int("epochSize", 100, "epoch size for MAB")
 	keyLen := flag.Int64("keyLen", 8, "length of key")
