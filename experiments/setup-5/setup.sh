e195bd280357932fd77abab739182339154df7c3
Pasindu Tennage <pasindu.13@cse.mrt.ac.lk>
2023-05-31 15:44:10 +0200

    minor changes, setup-5 checked

--- experiments/setup-5/setup.sh
+++ experiments/setup-5/setup.sh
@@ -7,8 +7,8 @@ replica_home_path="/home/${user_name}/raxos/"
 
 # generate config files
 
-python3 experiments/python/quepaxa-config.py 5 5 ${replica1} ${replica2} ${replica3} ${replica4} ${replica5} ${client1} ${client2} ${client3} ${client4} ${client5} > experiments/binary/quepaxa.yml
-python3 experiments/python/paxos-config.py   5 5 ${replica1} ${replica2} ${replica3} ${replica4} ${replica5} ${client1} ${client2} ${client3} ${client4} ${client5} > experiments/binary/paxos.yml
+python3 experiments/python/quepaxa-config.py 5 5 ${replica1_ip} ${replica2_ip} ${replica3_ip} ${replica4_ip} ${replica5_ip} ${client1_ip} ${client2_ip} ${client3_ip} ${client4_ip} ${client5_ip} > experiments/binary/quepaxa.yml
+python3 experiments/python/paxos-config.py   5 5 ${replica1_ip} ${replica2_ip} ${replica3_ip} ${replica4_ip} ${replica5_ip} ${client1_ip} ${client2_ip} ${client3_ip} ${client4_ip} ${client5_ip} > experiments/binary/paxos.yml
 
 
 for i in "${machines[@]}"
