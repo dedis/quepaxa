760f0f35b82b97290cf365761b8ef8592d91728f
Pasindu Tennage <pasindu.13@cse.mrt.ac.lk>
2023-06-02 20:29:38 +0200

    ran asynchrony/paxos_raft.sh

--- experiments/asynchrony/paxos_raft.sh
+++ experiments/asynchrony/paxos_raft.sh
@@ -52,7 +52,7 @@ sleep 5
 nohup sshpass ssh -o "StrictHostKeyChecking no" -i ${cert} ${client2} ".${remote_ctl_path} --name 22 --config ${remote_config_path} --logFilePath ${remote_log_path} --requestType status --operationType 3" >${output_path}status3.log &
 echo "Sent consensus start up"
 
-sleep 35
+sleep 15
 
 echo "Starting client[s]"
 
