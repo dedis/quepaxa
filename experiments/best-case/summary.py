a6234720f845dd0dd1be93cd445144c6070f7532
Pasindu Tennage <pasindu.13@cse.mrt.ac.lk>
2023-06-02 16:15:40 +0200

    checked summary.py after adding graphs

--- experiments/best-case/summary.py
+++ experiments/best-case/summary.py
@@ -41,7 +41,7 @@ if setting == "WAN":
 def getEPaxosSummary():
     l_records = []
     for arrival in arrivals:
-        record = ["epaxos-exec", str(arrival*5)]
+        record = ["epaxos-exec", str(arrival * 5)]
         throughput, latency, nine9, err = [], [], [], []
         for iteration in iterations:
             root = "experiments/best-case/logs/epaxos/" + str(arrival) + "/" + str(int(replicaBatchSize)) \
@@ -51,13 +51,13 @@ def getEPaxosSummary():
             latency.append(l)
             nine9.append(n)
             err.append(e)
-        record.append(int(sum(remove_farthest_from_median(throughput,1)) / (len(iterations)-1)))
-        record.append(int(sum(remove_farthest_from_median(latency,1)) / (len(iterations)-1)))
-        record.append(int(sum(remove_farthest_from_median(nine9,1)) / (len(iterations)-1)))
-        record.append(int(sum(remove_farthest_from_median(err,1)) / (len(iterations)-1)))
+        record.append(int(sum(remove_farthest_from_median(throughput, 1)) / (len(iterations) - 1)))
+        record.append(int(sum(remove_farthest_from_median(latency, 1)) / (len(iterations) - 1)))
+        record.append(int(sum(remove_farthest_from_median(nine9, 1)) / (len(iterations) - 1)))
+        record.append(int(sum(remove_farthest_from_median(err, 1)) / (len(iterations) - 1)))
         l_records.append(record)
 
-        record = ["epaxos-commit", str(arrival*5)]
+        record = ["epaxos-commit", str(arrival * 5)]
         throughput, latency, nine9, err = [], [], [], []
         for iteration in iterations:
             root = "experiments/best-case/logs/epaxos/" + str(arrival) + "/" + str(int(replicaBatchSize)) \
@@ -67,10 +67,10 @@ def getEPaxosSummary():
             latency.append(l)
             nine9.append(n)
             err.append(e)
-        record.append(int(sum(remove_farthest_from_median(throughput,1)) / (len(iterations)-1)))
-        record.append(int(sum(remove_farthest_from_median(latency,1)) / (len(iterations)-1)))
-        record.append(int(sum(remove_farthest_from_median(nine9,1)) / (len(iterations)-1)))
-        record.append(int(sum(remove_farthest_from_median(err,1)) / (len(iterations)-1)))
+        record.append(int(sum(remove_farthest_from_median(throughput, 1)) / (len(iterations) - 1)))
+        record.append(int(sum(remove_farthest_from_median(latency, 1)) / (len(iterations) - 1)))
+        record.append(int(sum(remove_farthest_from_median(nine9, 1)) / (len(iterations) - 1)))
+        record.append(int(sum(remove_farthest_from_median(err, 1)) / (len(iterations) - 1)))
         l_records.append(record)
 
     return l_records
@@ -79,20 +79,20 @@ def getEPaxosSummary():
 def getPaxosSummary():
     l_records = []
     for arrival in arrivals:
-        record = ["paxos", str(arrival*5)]
+        record = ["paxos", str(arrival * 5)]
         throughput, latency, nine9, err = [], [], [], []
         for iteration in iterations:
-            root= "experiments/best-case/logs/paxos/" + str(arrival) + "/" + str(int(replicaBatchSize)) \
-            + "/" + str(replicaBatchTime) + "/" + str(setting) + "/" + str(iteration) + "/execution/"
+            root = "experiments/best-case/logs/paxos/" + str(arrival) + "/" + str(int(replicaBatchSize)) \
+                   + "/" + str(replicaBatchTime) + "/" + str(setting) + "/" + str(iteration) + "/execution/"
             t, l, n, e = getEPaxosPaxosPerformance(root, 7, 5)
             throughput.append(t)
             latency.append(l)
             nine9.append(n)
             err.append(e)
-        record.append(int(sum(remove_farthest_from_median(throughput,1)) / (len(iterations)-1)))
-        record.append(int(sum(remove_farthest_from_median(latency,1)) / (len(iterations)-1)))
-        record.append(int(sum(remove_farthest_from_median(nine9,1)) / (len(iterations)-1)))
-        record.append(int(sum(remove_farthest_from_median(err,1)) / (len(iterations)-1)))
+        record.append(int(sum(remove_farthest_from_median(throughput, 1)) / (len(iterations) - 1)))
+        record.append(int(sum(remove_farthest_from_median(latency, 1)) / (len(iterations) - 1)))
+        record.append(int(sum(remove_farthest_from_median(nine9, 1)) / (len(iterations) - 1)))
+        record.append(int(sum(remove_farthest_from_median(err, 1)) / (len(iterations) - 1)))
         l_records.append(record)
     return l_records
 
@@ -100,7 +100,7 @@ def getPaxosSummary():
 def getQuePaxaSummary():
     l_records = []
     for arrival in arrivals:
-        record = ["raxos-0", str(arrival*5)]
+        record = ["quepaxa-0", str(arrival * 5)]
         throughput, latency, nine9, err = [], [], [], []
         for iteration in iterations:
             root = "experiments/best-case/logs/quepaxa/" + str(arrival) + "/" + str(int(replicaBatchSize)) + "/" + str(
@@ -111,13 +111,13 @@ def getQuePaxaSummary():
             latency.append(l)
             nine9.append(n)
             err.append(e)
-        record.append(int(sum(remove_farthest_from_median(throughput,1)) / (len(iterations)-1)))
-        record.append(int(sum(remove_farthest_from_median(latency,1)) / (len(iterations)-1)))
-        record.append(int(sum(remove_farthest_from_median(nine9,1)) / (len(iterations)-1)))
-        record.append(int(sum(remove_farthest_from_median(err,1)) / (len(iterations)-1)))
+        record.append(int(sum(remove_farthest_from_median(throughput, 1)) / (len(iterations) - 1)))
+        record.append(int(sum(remove_farthest_from_median(latency, 1)) / (len(iterations) - 1)))
+        record.append(int(sum(remove_farthest_from_median(nine9, 1)) / (len(iterations) - 1)))
+        record.append(int(sum(remove_farthest_from_median(err, 1)) / (len(iterations) - 1)))
         l_records.append(record)
 
-        record = ["raxos-1", str(arrival*5)]
+        record = ["quepaxa-1", str(arrival * 5)]
         throughput, latency, nine9, err = [], [], [], []
         for iteration in iterations:
             root = "experiments/best-case/logs/quepaxa/" + str(arrival) + "/" + str(int(replicaBatchSize)) + "/" + str(
@@ -128,10 +128,10 @@ def getQuePaxaSummary():
             latency.append(l)
             nine9.append(n)
             err.append(e)
-        record.append(int(sum(remove_farthest_from_median(throughput,1)) / (len(iterations)-1)))
-        record.append(int(sum(remove_farthest_from_median(latency,1)) / (len(iterations)-1)))
-        record.append(int(sum(remove_farthest_from_median(nine9,1)) / (len(iterations)-1)))
-        record.append(int(sum(remove_farthest_from_median(err,1)) / (len(iterations)-1)))
+        record.append(int(sum(remove_farthest_from_median(throughput, 1)) / (len(iterations) - 1)))
+        record.append(int(sum(remove_farthest_from_median(latency, 1)) / (len(iterations) - 1)))
+        record.append(int(sum(remove_farthest_from_median(nine9, 1)) / (len(iterations) - 1)))
+        record.append(int(sum(remove_farthest_from_median(err, 1)) / (len(iterations) - 1)))
         l_records.append(record)
     return l_records
 
@@ -143,7 +143,7 @@ ePaxosSummary = getEPaxosSummary()
 paxosSummary = getPaxosSummary()
 quePaxaSummary = getQuePaxaSummary()
 
-records = records+ePaxosSummary+paxosSummary+quePaxaSummary
+records = records + ePaxosSummary + paxosSummary + quePaxaSummary
 
 import csv
 
@@ -151,108 +151,110 @@ with open("experiments/best-case/logs/summary.csv", "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerows(records)
 
-# epaxos_exec_throughput = []
-# epaxos_exec_latency = []
-# epaxos_exec_tail = []
-#
-# epaxos_no_exec_throughput = []
-# epaxos_no_exec_latency = []
-# epaxos_no_exec_tail = []
-#
-# for e in ePaxosSummary:
-#     records.append(e)
-#     if e[0] == "epaxos-commit":
-#         epaxos_no_exec_throughput.append(e[2])
-#         epaxos_no_exec_latency.append(e[3])
-#         epaxos_no_exec_tail.append(e[4])
-#     elif e[0] == "epaxos-exec":
-#         if 900 < float(e[1]) < 1100:
-#             continue
-#         epaxos_exec_throughput.append(e[2])
-#         epaxos_exec_latency.append(e[3])
-#         epaxos_exec_tail.append(e[4])
-#     else:
-#         exit("should not happen")
-#
-#
-#
-# paxos_throughput = []
-# paxos_latency = []
-# paxos_tail = []
-#
-# for p in paxosSummary:
-#     records.append(p)
-#     paxos_throughput.append(p[2])
-#     paxos_latency.append(p[3])
-#     paxos_tail.append(p[4])
-#
-# raftSummary = getRaftSummary()
-# for r in raftSummary:
-#     records.append(r)
-#
-#
-#
-# raxos_lan_throughput = []
-# raxos_lan_latency = []
-# raxos_lan_tail = []
-#
-# for ra in raxosSummary:
-#     records.append(ra)
-#     if ra[0] == "raxos-1":
-#         raxos_lan_throughput.append(ra[2])
-#         raxos_lan_latency.append(ra[3])
-#         raxos_lan_tail.append(ra[4])
+epaxos_exec_throughput = []
+epaxos_exec_latency = []
+epaxos_exec_tail = []
+
+epaxos_no_exec_throughput = []
+epaxos_no_exec_latency = []
+epaxos_no_exec_tail = []
+
+for e in ePaxosSummary:
+    if e[0] == "epaxos-commit":
+        epaxos_no_exec_throughput.append(e[2])
+        epaxos_no_exec_latency.append(e[3])
+        epaxos_no_exec_tail.append(e[4])
+    elif e[0] == "epaxos-exec":
+        if 1000 < float(e[1]) < 6000:
+            continue  # arrival rate 5000 requires a low batch size
+        epaxos_exec_throughput.append(e[2])
+        epaxos_exec_latency.append(e[3])
+        epaxos_exec_tail.append(e[4])
+    else:
+        exit("should not happen")
+
+paxos_throughput = []
+paxos_latency = []
+paxos_tail = []
+
+for p in paxosSummary:
+    paxos_throughput.append(p[2])
+    paxos_latency.append(p[3])
+    paxos_tail.append(p[4])
+
+quepaxa_throughput = []
+quepaxa_latency = []
+quepaxa_tail = []
+
+for ra in quePaxaSummary:
+    if ra[0] == "quepaxa-1":
+        quepaxa_throughput.append(ra[2])
+        quepaxa_latency.append(ra[3])
+        quepaxa_tail.append(ra[4])
+
+import matplotlib.pyplot as plt
 
 
-#
-# import matplotlib.pyplot as plt
-#
-#
-# def di_func(array):
-#     returnList = []
-#     for l in array:
-#         returnList.append(l / 1000)
-#     return returnList
-#
-# # ninty latency
-#
-# plt.figure(figsize=(5, 4))
-# plt.rcParams.update({'font.size': 11.00})
-# ax = plt.gca()
-# ax.grid()
-# ax.set_xlim([0, 350])
-# ax.set_ylim([0, 80])
-#
-# plt.plot(di_func(raxos_lan_throughput), di_func(raxos_lan_tail), 'b.-', label="QuePaxa")
-# plt.plot(di_func(paxos_throughput), di_func(paxos_tail), 'y*-', label="Multi-Paxos")
-# plt.plot(di_func(epaxos_no_exec_throughput), di_func(epaxos_no_exec_tail), 'cx-', label="Epaxos-commit")
-# plt.plot(di_func(epaxos_exec_throughput), di_func(epaxos_exec_tail), 'm-.', label="Epaxos-exec")
-#
-# plt.xlabel('Throughput (x 1k cmd/sec)')
-# plt.ylabel('99 percentile Latency (ms)')
-# plt.legend()
-# plt.savefig('aws/best-case-lan/throughput_tail.pdf')
-# plt.close()
-# plt.clf()
-# plt.cla()
-#
-# # median latency
-#
-# plt.figure(figsize=(5, 4))
-# plt.rcParams.update({'font.size': 11.00})
-# ax = plt.gca()
-# ax.grid()
-# ax.set_xlim([0, 700])
-# ax.set_ylim([1, 7])
-# plt.plot(di_func(raxos_lan_throughput), di_func(raxos_lan_latency), 'b.-', label="QuePaxa")
-# plt.plot(di_func(paxos_throughput), di_func(paxos_latency), 'y*-', label="Multi-Paxos")
-# plt.plot(di_func(epaxos_no_exec_throughput), di_func(epaxos_no_exec_latency), 'cx-', label="Epaxos-commit")
-# plt.plot(di_func(epaxos_exec_throughput), di_func(epaxos_exec_latency), 'm-', label="Epaxos-exec")
-#
-# plt.xlabel('Throughput (x 1k cmd/sec)')
-# plt.ylabel('Median Latency (ms)')
-# plt.legend()
-# plt.savefig('aws/best-case-lan/throughput_median.pdf')
-# plt.close()
-# plt.clf()
-# plt.cla()
+def di_func(array):
+    returnList = []
+    for l in array:
+        returnList.append(l / 1000)
+    return returnList
+
+
+# ninty latency
+
+plt.figure(figsize=(5, 4))
+plt.rcParams.update({'font.size': 14.30})
+ax = plt.gca()
+ax.grid()
+
+if setting == "LAN":
+    ax.set_xlim([0, 350])
+    ax.set_ylim([0, 80])
+
+if setting == "WAN":
+    ax.set_xlim([0, 390])
+    ax.set_ylim([0, 5000])
+
+
+plt.plot(di_func(quepaxa_throughput), di_func(quepaxa_tail), 'b.-', label="QuePaxa")
+plt.plot(di_func(paxos_throughput), di_func(paxos_tail), 'y*-', label="Multi-Paxos")
+plt.plot(di_func(epaxos_no_exec_throughput), di_func(epaxos_no_exec_tail), 'cx-', label="Epaxos-commit")
+plt.plot(di_func(epaxos_exec_throughput), di_func(epaxos_exec_tail), 'mo-.', label="Epaxos-exec")
+
+plt.xlabel('Throughput (x 1k cmd/sec)')
+plt.ylabel('99 percentile Latency (ms)')
+plt.legend()
+plt.savefig('experiments/best-case/logs/throughput_tail'+setting+'.pdf', bbox_inches='tight', pad_inches=0)
+plt.close()
+plt.clf()
+plt.cla()
+
+# median latency
+
+plt.figure(figsize=(5, 4))
+plt.rcParams.update({'font.size': 14.30})
+ax = plt.gca()
+ax.grid()
+
+if setting == "LAN":
+    ax.set_xlim([0, 700])
+    ax.set_ylim([0, 7])
+
+if setting == "WAN":
+    ax.set_xlim([0, 360])
+    ax.set_ylim([0, 3000])
+
+plt.plot(di_func(quepaxa_throughput), di_func(quepaxa_latency), 'b.-', label="QuePaxa")
+plt.plot(di_func(paxos_throughput), di_func(paxos_latency), 'y*-', label="Multi-Paxos")
+plt.plot(di_func(epaxos_no_exec_throughput), di_func(epaxos_no_exec_latency), 'cx-', label="Epaxos-commit")
+plt.plot(di_func(epaxos_exec_throughput), di_func(epaxos_exec_latency), 'mo-', label="Epaxos-exec")
+
+plt.xlabel('Throughput (x 1k cmd/sec)')
+plt.ylabel('Median Latency (ms)')
+plt.legend()
+plt.savefig('experiments/best-case/logs/throughput_median'+setting+'.pdf', bbox_inches='tight', pad_inches=0)
+plt.close()
+plt.clf()
+plt.cla()
