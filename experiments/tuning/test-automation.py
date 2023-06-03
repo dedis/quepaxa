e6b838f3421fa90d174c65855228016d2f46c893
Pasindu Tennage <pasindu.13@cse.mrt.ac.lk>
2023-06-04 00:16:01 +0200

    added and checked test-automation.py

---
+++ experiments/tuning/test-automation.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+numIter = sys.argv[1]
+
+if int(numIter) < 4:
+    exit("at least 4 iterations needed")
+
+os.system("/bin/bash experiments/setup-5/setup.sh")
+
+iterations = list(range(1, int(numIter) + 1))
+arrivals = [80000, 100000, 120000]
+
+for iteration in iterations:
+    for arrival in arrivals:
+        for algo in ["paxos", "raft"]:
+            os.system(
+                "/bin/bash experiments/tuning/paxos_raft.sh " + str(algo) + " "
+                + str(iteration) + " "
+                + str(arrival))
+
+        os.system(
+            "/bin/bash experiments/tuning/quepaxa.sh " + str(iteration) + " "
+            + str(arrival))
