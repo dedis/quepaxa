4528fb57717d17878078d1288916c1e40240a223
Pasindu Tennage <pasindu.13@cse.mrt.ac.lk>
2023-06-18 16:22:23 +0200

    asynchrony all files checked

--- experiments/asynchrony/asynchrony.sh
+++ experiments/asynchrony/asynchrony.sh
@@ -3,5 +3,5 @@ pwd=$(pwd)
 
 echo "Asynchrony tests"
 
-python3 experiment/asynchrony/test_automation.py 5
-python3 experiment/asynchrony/summary.py 5 ${device}
+python3 experiments/asynchrony/test_automation.py 5
+python3 experiments/asynchrony/summary.py 5 ${device}
