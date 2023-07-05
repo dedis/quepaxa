f62b8a0b636de35352fb759fec771e040407aab8
Pasindu Tennage <pasindu.13@cse.mrt.ac.lk>
2023-07-05 21:21:15 +0200

    added and checked rabia performance extract

--- experiments/python/performance_extract.py
+++ experiments/python/performance_extract.py
@@ -106,3 +106,33 @@ def remove_farthest_from_median(A, n):
     cleaned_A = np.delete(A, sorted_indices[:n])
 
     return cleaned_A
+
+def getRabiaPerformance(root, initClient, numClients):
+    throughputs = []
+    medians = []
+    ninety9s = []
+    errors = []
+    for cl in list(range(initClient, initClient+numClients, 1)):
+        file_name = root + str(cl) + ".log"
+
+        try:
+            f = open(file_name, 'r')
+        except OSError:
+            print("Error in " + file_name + "\n")
+            return [-1, -1, -1, -1]
+        with f:
+            content = f.readlines()
+        if len(content) < 4:
+            print("Error in " + file_name + "\n")
+            return [-1, -1, -1, -1]
+        if content[0].strip().split(" ")[0] == "Warning:":
+            content = content[1:]
+        if not (content[1].strip().split(" ")[0] == "Throughput" and content[2].strip().split(" ")[0] == "Median"):
+            print("Error in " + file_name + "\n")
+            return [-1, -1, -1, -1]
+        throughputs.append(float(content[1].strip().split(" ")[2]))
+        medians.append(float(content[2].strip().split(" ")[3]))
+        ninety9s.append(float(content[3].strip().split(" ")[4]))
+        errors.append(float(content[4].strip().split(" ")[3]))
+
+    return [sum(throughputs), sum(medians) / numClients, sum(ninety9s) / numClients, sum(errors)]
