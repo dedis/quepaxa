9382bc9c8cb194c50511cb6ef4c04a21b9e067f3
Pasindu Tennage <pasindu.13@cse.mrt.ac.lk>
2023-07-19 14:46:26 +0200

    checked experiments/python/performance_extract

--- experiments/python/performance_extract.py
+++ experiments/python/performance_extract.py
@@ -1,28 +1,31 @@
+import sys
+
 import numpy as np
+
 
 def getPaxosRaftPerformance(root, initClient, numClients):
     throughputs = []
     medians = []
     ninety9s = []
     errors = []
-    for cl in list(range(initClient, initClient+numClients, 1)):
+    for cl in list(range(initClient, initClient + numClients, 1)):
         file_name = root + str(cl) + ".log"
         # print(file_name + "\n")
         try:
             f = open(file_name, 'r')
         except OSError:
-            print("Error in " + file_name + "\n")
-            return [-1, -1, -1, -1]
+            sys.exit("Error in " + file_name + "\n")
+
         with f:
             content = f.readlines()
         if len(content) < 8:
-            print("Error in " + file_name + "\n")
-            return [-1, -1, -1, -1]
+            sys.exit("Error in " + file_name + "\n")
+
         if content[0].strip().split(" ")[0] == "Warning:":
             content = content[1:]
         if not (content[4].strip().split(" ")[0] == "Throughput" and content[5].strip().split(" ")[0] == "Median"):
-            print("Error in " + file_name + "\n")
-            return [-1, -1, -1, -1]
+            sys.exit("Error in " + file_name + "\n")
+
         throughputs.append(float(content[4].strip().split(" ")[5]))
         medians.append(float(content[5].strip().split(" ")[3]))
         ninety9s.append(float(content[6].strip().split(" ")[4]))
@@ -36,24 +39,24 @@ def getQuePaxaPerformance(root, initClient, numClients):
     medians = []
     ninety9s = []
     errors = []
-    for cl in list(range(initClient, initClient+numClients, 1)):
+    for cl in list(range(initClient, initClient + numClients, 1)):
         file_name = root + str(cl) + ".log"
         # print(file_name + "\n")
         try:
             f = open(file_name, 'r')
         except OSError:
-            print("Error in " + file_name + "\n")
-            return [-1, -1, -1, -1]
+            sys.exit("Error in " + file_name + "\n")
+
         with f:
             content = f.readlines()
         if len(content) < 10:
-            print("Error in " + file_name + "\n")
-            return [-1, -1, -1, -1]
+            sys.exit("Error in " + file_name + "\n")
+
         if content[0].strip().split(" ")[0] == "Warning:":
             content = content[1:]
         if not (content[6].strip().split(" ")[0] == "Throughput" and content[7].strip().split(" ")[0] == "Median"):
-            print("Error in " + file_name + "\n")
-            return [-1, -1, -1, -1]
+            sys.exit("Error in " + file_name + "\n")
+
         throughputs.append(float(content[6].strip().split(" ")[2]))
         medians.append(float(content[7].strip().split(" ")[3]))
         ninety9s.append(float(content[8].strip().split(" ")[4]))
@@ -67,24 +70,24 @@ def getEPaxosPaxosPerformance(root, initClient, numClients):
     medians = []
     ninety9s = []
     errors = []
-    for cl in list(range(initClient, initClient+numClients, 1)):
+    for cl in list(range(initClient, initClient + numClients, 1)):
         file_name = root + str(cl) + ".log"
         # print(file_name + "\n")
         try:
             f = open(file_name, 'r')
         except OSError:
-            print("Error in " + file_name + "\n")
-            return [-1, -1, -1, -1]
+            sys.exit("Error in " + file_name + "\n")
+
         with f:
             content = f.readlines()
         if len(content) < 5:
-            print("Error in " + file_name + "\n")
-            return [-1, -1, -1, -1]
+            sys.exit("Error in " + file_name + "\n")
+
         if content[0].strip().split(" ")[0] == "Warning:":
             content = content[1:]
         if not (content[1].strip().split(" ")[0] == "Throughput" and content[2].strip().split(" ")[0] == "Median"):
-            print("Error in " + file_name + "\n")
-            return [-1, -1, -1, -1]
+            sys.exit("Error in " + file_name + "\n")
+
         throughputs.append(float(content[1].strip().split(" ")[5]))
         medians.append(float(content[2].strip().split(" ")[3]))
         ninety9s.append(float(content[3].strip().split(" ")[4]))
@@ -92,6 +95,36 @@ def getEPaxosPaxosPerformance(root, initClient, numClients):
 
     return [sum(throughputs), sum(medians) / numClients, sum(ninety9s) / numClients, sum(errors)]
 
+
+def getRabiaPerformance(root, initClient, numClients):
+    throughputs = []
+    medians = []
+    ninety9s = []
+    errors = []
+    for cl in list(range(initClient, initClient + numClients, 1)):
+        file_name = root + str(cl) + ".log"
+
+        try:
+            f = open(file_name, 'r')
+        except OSError:
+            sys.exit("Error in " + file_name + "\n")
+
+        with f:
+            content = f.readlines()
+        if len(content) < 5:
+            sys.exit("Error in " + file_name + "\n")
+
+        if content[0].strip().split(" ")[0] == "Warning:":
+            content = content[1:]
+        if not (content[1].strip().split(" ")[0] == "Throughput" and content[2].strip().split(" ")[0] == "Median"):
+            sys.exit("Error in " + file_name + "\n")
+
+        throughputs.append(float(content[1].strip().split(" ")[2]))
+        medians.append(float(content[2].strip().split(" ")[3]))
+        ninety9s.append(float(content[3].strip().split(" ")[4]))
+        errors.append(float(content[4].strip().split(" ")[3]))
+
+    return [sum(throughputs), sum(medians) / numClients, sum(ninety9s) / numClients, sum(errors)]
 
 def remove_farthest_from_median(A, n):
     median = np.median(A)
@@ -106,33 +139,3 @@ def remove_farthest_from_median(A, n):
     cleaned_A = np.delete(A, sorted_indices[:n])
 
     return cleaned_A
-
-def getRabiaPerformance(root, initClient, numClients):
-    throughputs = []
-    medians = []
-    ninety9s = []
-    errors = []
-    for cl in list(range(initClient, initClient+numClients, 1)):
-        file_name = root + str(cl) + ".log"
-
-        try:
-            f = open(file_name, 'r')
-        except OSError:
-            print("Error in " + file_name + "\n")
-            return [-1, -1, -1, -1]
-        with f:
-            content = f.readlines()
-        if len(content) < 4:
-            print("Error in " + file_name + "\n")
-            return [-1, -1, -1, -1]
-        if content[0].strip().split(" ")[0] == "Warning:":
-            content = content[1:]
-        if not (content[1].strip().split(" ")[0] == "Throughput" and content[2].strip().split(" ")[0] == "Median"):
-            print("Error in " + file_name + "\n")
-            return [-1, -1, -1, -1]
-        throughputs.append(float(content[1].strip().split(" ")[2]))
-        medians.append(float(content[2].strip().split(" ")[3]))
-        ninety9s.append(float(content[3].strip().split(" ")[4]))
-        errors.append(float(content[4].strip().split(" ")[3]))
-
-    return [sum(throughputs), sum(medians) / numClients, sum(ninety9s) / numClients, sum(errors)]
