fcb25b34ced3a2cb5737bd1b55452ba380460698
Pasindu Tennage <pasindu.13@cse.mrt.ac.lk>
2023-07-20 18:54:08 +0200

    checked experiments/scalability-13/

--- experiments/scalability-13/test-automation.py
+++ experiments/scalability-13/test-automation.py
@@ -8,8 +8,8 @@ from performance_extract import *
 
 numIter = sys.argv[1]
 
-if int(numIter) < 4:
-    exit("at least 4 iterations needed")
+if int(numIter) < 3:
+    exit("at least 3 iterations needed")
 
 os.system("/bin/bash experiments/setup-13/setup.sh")
 
@@ -19,31 +19,8 @@ arrivals = [467337, 380000]
 for iteration in iterations:
     for arrival in arrivals:
         os.system(
-            "/bin/bash experiments/scalability-13/paxos.sh " + str(int(arrival)) + " "
-            + str(iteration))
-
-        os.system(
             "/bin/bash experiments/scalability-13/quepaxa.sh " + str(int(arrival)) + " "
             + str(iteration))
-
-def getPaxosSummary():
-    l_records = []
-    for arrival in arrivals:
-        record = ["paxos", str(arrival * 13)]
-        throughput, latency, nine9, err = [], [], [], []
-        for iteration in iterations:
-            root = "experiments/scalability-13/logs/paxos/" + str(arrival) + "/" + str(int(iteration))+"/"
-            t, l, n, e = getEPaxosPaxosPerformance(root, 15, 13)
-            throughput.append(t)
-            latency.append(l)
-            nine9.append(n)
-            err.append(e)
-        record.append(int(sum(remove_farthest_from_median(throughput, 1)) / (len(iterations) - 1)))
-        record.append(int(sum(remove_farthest_from_median(latency, 1)) / (len(iterations) - 1)))
-        record.append(int(sum(remove_farthest_from_median(nine9, 1)) / (len(iterations) - 1)))
-        record.append(int(sum(remove_farthest_from_median(err, 1)) / (len(iterations) - 1)))
-        l_records.append(record)
-    return l_records
 
 
 def getQuePaxaSummary():
@@ -65,5 +42,4 @@ def getQuePaxaSummary():
         l_records.append(record)
     return l_records
 
-print(getPaxosSummary())
 print(getQuePaxaSummary())
